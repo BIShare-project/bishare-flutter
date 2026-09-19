d5c55f7d5a10d040ca0d8c5a06d02bcaa91160d6
bishare-release <support@billiongroup.net>
2026-09-19 02:48:59 +0000

    packaging: manifests for v2.5.7 [skip ci]

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -4,8 +4,8 @@ $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $packageArgs = @{
   packageName    = 'bishare'
   unzipLocation  = $toolsDir
-  url64bit       = 'https://github.com/BIShare-project/bishare-flutter/releases/download/v2.5.6/BIShare-2.5.6-windows-x64.zip'
-  checksum64     = '2db3bc32f0eb932950b79be8e58786eae30291f771d66dbd3b89ec8c70c05e0c'
+  url64bit       = 'https://github.com/BIShare-project/bishare-flutter/releases/download/v2.5.7/BIShare-2.5.7-windows-x64.zip'
+  checksum64     = 'ed91d654ec6f5ed46be17665d72ecea5d7f101f0ed840d4c4555fa30b0ca1cf1'
   checksumType64 = 'sha256'
 }
 Install-ChocolateyZipPackage @packageArgs
--- packaging/homebrew/bishare.rb
+++ packaging/homebrew/bishare.rb
@@ -1,6 +1,6 @@
 cask "bishare" do
-  version "2.5.6"
-  sha256 "1a35ab03eed8f4455581ce2a1883b070a0f31243b08fa4c5f2ee7fd4ebb6b47d" # filled by packaging/bump.sh
+  version "2.5.7"
+  sha256 "86aa9c3160d69d244ea67a57a82e43895e70ff62e11936d940b92fcb2fc8ff40" # filled by packaging/bump.sh
 
   url "https://github.com/BIShare-project/bishare-flutter/releases/download/v#{version}/BIShare-#{version}-macos.dmg",
       verified: "github.com/BIShare-project/bishare-flutter/"
